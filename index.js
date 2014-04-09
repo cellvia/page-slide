a5e03b928bad64c024e8e1301691b03c62d2d726
Brandon Selway <cellvia@yahoo.com>
2014-04-09 16:43:07 -0600

    git it going

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+var insertCss = require('insert-css');
+var fs = require('fs');
+var inserted;
 
-/* Notes:
- * - History management is currently done using window.location.hash.  This could easily be changed to use Push State instead.
- * - jQuery dependency for now. This could also be easily removed.
- */
+module.exports = function(container, options) {
 
-module.exports = function PageSlider(container) {
+    if(options.insertCss !== false && !inserted){
+        insertCss(fs.readFileSync(__dirname + "/css/style.css"));
+        inserted = true;
+    } 
 
     var container = container,
+        isJ = container instanceof jQuery,
         currentPage,
         stateHistory = [];
 
@@ -14,7 +18,7 @@ module.exports = function PageSlider(container) {
     this.slidePage = function(page) {
 
         var l = stateHistory.length,
-            state = window.location.hash;
+            state = window.history && history.pushState ? window.location.pathname : window.location.hash;
 
         if (l === 0) {
             stateHistory.push(state);
@@ -29,33 +33,234 @@ module.exports = function PageSlider(container) {
             this.slidePageFrom(page, 'right');
         }
 
-    }
+    };
 
     // Use this function directly if you want to control the sliding direction outside PageSlider
     this.slidePageFrom = function(page, from) {
 
-        container.append(page);
+        container[isJ ? "append" : appendChild](page);
 
         if (!currentPage || !from) {
-            page.classList.add("page center");
+            if(isJ){
+                page.removeClass("left right transition").addClass("page center");            
+            }else{
+                page.classList.remove("left", "right", "transition");
+                page.classList.add("page", "center");            
+            }
             currentPage = page;
             return;
         }
 
         // Position the page at the starting position of the animation
-        page.classList.add("page " + from);
+        var notFrom = from === "left" ? "right" : "left";
+        if(isJ){
+            page.removeClass(notFrom + " center transition").addClass("page " + from);
+        }else{
+            page.classList.remove("center", notFrom, "transition");
+            page.classList.add("page", from);
+        }
 
         currentPage.one('webkitTransitionEnd', function(e) {
-            e.target.remove();
+            if(isJ){
+                $(e.target).remove();            
+            }else{
+                e.target.parentNode.removeChild(e.target);
+            }
         });
 
         // Force reflow. More information here: http://www.phpied.com/rendering-repaint-reflowrelayout-restyle/
         container[0].offsetWidth;
 
         // Position the new page and the current page at the ending position of their animation with a transition class indicating the duration of the animation
-        page.classList.add("page transition center");
-        currentPage.classList.add("page transition " + (from === "left" ? "right" : "left"));
+        if(isJ){
+            page.removeClass("left right");
+            page.addClass("page transition center");
+            currentPage.removeClass("center " + (from === "left" ? "left" : "right"));
+            currentPage.addClass("page transition " + (from === "left" ? "right" : "left"));
+        }else{
+            page.classList.remove("right", "left");
+            page.classList.add("page", "transition", "center");
+            currentPage.classList.remove("center", from === "left" ? "left" : "right");
+            currentPage.classList.add("page", "transition", from === "left" ? "right" : "left");
+        }
         currentPage = page;
+    };
+
+}
+
+/*
+ * classList.js: Cross-browser full element.classList implementation.
+ * 2012-11-15
+ *
+ * By Eli Grey, http://eligrey.com
+ * Public Domain.
+ * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+ */
+
+/*global self, document, DOMException */
+
+/*! @source http://purl.eligrey.com/github/classList.js/blob/master/classList.js*/
+
+if (typeof document !== "undefined" && !("classList" in document.documentElement)) {
+
+(function (view) {
+
+"use strict";
+
+if (!('HTMLElement' in view) && !('Element' in view)) return;
+
+var
+      classListProp = "classList"
+    , protoProp = "prototype"
+    , elemCtrProto = (view.HTMLElement || view.Element)[protoProp]
+    , objCtr = Object
+    , strTrim = String[protoProp].trim || function () {
+        return this.replace(/^\s+|\s+$/g, "");
     }
+    , arrIndexOf = Array[protoProp].indexOf || function (item) {
+        var
+              i = 0
+            , len = this.length
+        ;
+        for (; i < len; i++) {
+            if (i in this && this[i] === item) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    // Vendors: please allow content code to instantiate DOMExceptions
+    , DOMEx = function (type, message) {
+        this.name = type;
+        this.code = DOMException[type];
+        this.message = message;
+    }
+    , checkTokenAndGetIndex = function (classList, token) {
+        if (token === "") {
+            throw new DOMEx(
+                  "SYNTAX_ERR"
+                , "An invalid or illegal string was specified"
+            );
+        }
+        if (/\s/.test(token)) {
+            throw new DOMEx(
+                  "INVALID_CHARACTER_ERR"
+                , "String contains an invalid character"
+            );
+        }
+        return arrIndexOf.call(classList, token);
+    }
+    , ClassList = function (elem) {
+        var
+              trimmedClasses = strTrim.call(elem.className)
+            , classes = trimmedClasses ? trimmedClasses.split(/\s+/) : []
+            , i = 0
+            , len = classes.length
+        ;
+        for (; i < len; i++) {
+            this.push(classes[i]);
+        }
+        this._updateClassName = function () {
+            elem.className = this.toString();
+        };
+    }
+    , classListProto = ClassList[protoProp] = []
+    , classListGetter = function () {
+        return new ClassList(this);
+    }
+;
+// Most DOMException implementations don't allow calling DOMException's toString()
+// on non-DOMExceptions. Error's toString() is sufficient here.
+DOMEx[protoProp] = Error[protoProp];
+classListProto.item = function (i) {
+    return this[i] || null;
+};
+classListProto.contains = function (token) {
+    token += "";
+    return checkTokenAndGetIndex(this, token) !== -1;
+};
+classListProto.add = function () {
+    var
+          tokens = arguments
+        , i = 0
+        , l = tokens.length
+        , token
+        , updated = false
+    ;
+    do {
+        token = tokens[i] + "";
+        if (checkTokenAndGetIndex(this, token) === -1) {
+            this.push(token);
+            updated = true;
+        }
+    }
+    while (++i < l);
+
+    if (updated) {
+        this._updateClassName();
+    }
+};
+classListProto.remove = function () {
+    var
+          tokens = arguments
+        , i = 0
+        , l = tokens.length
+        , token
+        , updated = false
+    ;
+    do {
+        token = tokens[i] + "";
+        var index = checkTokenAndGetIndex(this, token);
+        if (index !== -1) {
+            this.splice(index, 1);
+            updated = true;
+        }
+    }
+    while (++i < l);
+
+    if (updated) {
+        this._updateClassName();
+    }
+};
+classListProto.toggle = function (token, forse) {
+    token += "";
+
+    var
+          result = this.contains(token)
+        , method = result ?
+            forse !== true && "remove"
+        :
+            forse !== false && "add"
+    ;
+
+    if (method) {
+        this[method](token);
+    }
+
+    return !result;
+};
+classListProto.toString = function () {
+    return this.join(" ");
+};
+
+if (objCtr.defineProperty) {
+    var classListPropDesc = {
+          get: classListGetter
+        , enumerable: true
+        , configurable: true
+    };
+    try {
+        objCtr.defineProperty(elemCtrProto, classListProp, classListPropDesc);
+    } catch (ex) { // IE 8 doesn't support enumerable:true
+        if (ex.number === -0x7FF5EC54) {
+            classListPropDesc.enumerable = false;
+            objCtr.defineProperty(elemCtrProto, classListProp, classListPropDesc);
+        }
+    }
+} else if (objCtr[protoProp].__defineGetter__) {
+    elemCtrProto.__defineGetter__(classListProp, classListGetter);
+}
+
+}(self));
 
 }
--- readme.md
+++ readme.md
@@ -1,5 +1,14 @@
 # PageSlide: Hardware Accelerated Page Transitions for Mobile Web Apps
 
-A Browserify ready implmentation of @coenraets PageSlide. More info here:
+A Browserify ready implmentation of @coenraets PageSlide. 
+
+This is a rework of https://github.com/brianleroux/page-slide
+
+-native AND jQuery compatibile
+-pushState detection
+-default css optionally auto-inserted via [insert-css](http://github.com/substack/insert-css) and [brfs](http://github.com/substack/brfs) 
+
+
+More info here:
 
 http://coenraets.org/blog/2013/03/hardware-accelerated-page-transitions-for-mobile-web-apps-phonegap-apps/
